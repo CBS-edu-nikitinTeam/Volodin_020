60779e0dfb11723372c3f7c6c367636d4e19222f
Volodich <vovasnikitin12fc@gmail.com>
2021-01-31 20:14:48 +0200

    review by Volodich

--- Exercise2/ArithmeticFunc.cs
+++ Exercise2/ArithmeticFunc.cs
@@ -7,6 +7,7 @@ namespace Exercise2
     delegate double Operation(double operand1, double operand2);
     class ArithmeticFunc
     {
+        // Я бы всё таки вынес не в свойство AllOPeration, а в метод, который возвращает делегат операции. 
         public static Dictionary<char, Operation> AllOperations
         {
             get
@@ -16,6 +17,8 @@ namespace Exercise2
                     { '+', (double o1, double o2) => o1 + o2 },
                     { '-', (double o1, double o2) => o1 - o2 },
                     { '*', (double o1, double o2) => o1 * o2 },
+                    // Интересно что ошибки не будет при делении на ноль :)
+                    // https://docs.microsoft.com/ru-ru/dotnet/api/system.double.nan?view=net-5.0
                     { '/', (double o1, double o2) => { return o2 != 0 ? o1 / o2 : throw new DivideByZeroException(); } }
                 };
             }
